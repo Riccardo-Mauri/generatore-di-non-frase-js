3c29e5164e67b2c437e4904612a77c067fc3b266
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2025-03-11 13:55:32 +0100

    readme compilato, esercizio finito

--- scripts.js
+++ scripts.js
@@ -1,13 +1,6 @@
 //il mio codice javascript
-console.log("ciao da js");
+console.log("ciao da js!");
 
-/* 
-Il mio intento è di generare una frase casuale cliccando il bottone nella pagina, successivamente restituirla alla pagina e vederla.
-Pensavo di creare una sequenza di array con all'interno varie parole e,
-una volta premuto il bottone con una funzione di ascolto caricare da questi array randomicamente le parole al loro interno,
-da simulare una generazione casuale di parole,
-non mi interessa che abbia o no senso compiuto,il suo fine ultimo è semplicemente quello di creare una frase ogni volta che ci clicchi sopra
-*/
 //creo quindi i miei array che contengono parole casuali
 const primoArray = ["sole", "finestra", "libreria", "gatto", "tavolo"];
 const secondoArray = ["montagna", "telefono", "matita", "cuscino", "bicicletta"];
@@ -20,7 +13,7 @@ const fraseDiUscita = document.getElementById("FraseDiUscita");
 //aggiungo un adeventlistener per catturare ogni volta che il pulsante viene premuto
 
 mioBottone.addEventListener("click", function(){
-    console.log("bottone premuto");
+    console.log("bottone premuto!");
     //prendo la parola dal mio primo array
     const primaParola = primoArray[Math.floor(Math.random() * primoArray.length)];
     console.log(primaParola);
